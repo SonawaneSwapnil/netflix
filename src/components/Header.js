205978f160004c5ddca9273cafe9af5349c80d20
SwapnilSonawane <swapnils@atjoin.in>
2023-08-29 13:07:44 +0530

    Sign in,Sign up form,login page

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="absolute px-8 bg-gradient-to-b from-black z-10">
+      <img
+        className="w-36 "
+        src="https://images.ctfassets.net/4cd45et68cgf/7LrExJ6PAj6MSIPkDyCO86/542b1dfabbf3959908f69be546879952/Netflix-Brand-Logo.png?w=700&h=456"
+        alt="logo"
+      />
+    </div>
+  );
+}
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import Header from "./Header";
 
 export default function Login() {
-  return <div>Login</div>;
+  const [sign, setSign] = useState(true);
+  const toogleSignInform = () => {
+    setSign(!sign);
+  };
+  return (
+    <div>
+      <Header />
+      <img
+        className="absolute"
+        src="https://cdn.mos.cms.futurecdn.net/rDJegQJaCyGaYysj2g5XWY.jpg"
+        alt="bg-image"
+      />
+
+      <form className="absolute p-11 text-white bg-gray-900 w-3/12 my-36  mx-auto right-0 left-0 bg-opacity-90">
+        <h1 className="font-bold text-3xl py-4">
+          {sign ? "Sign in" : "Sign Up"}
+        </h1>
+        {!sign && (
+          <input
+            type="text"
+            name="text"
+            value=""
+            placeholder="Enter Full Name "
+            className="p-4 my-4 w-full bg-gray-700"
+          />
+        )}
+        <input
+          type="text"
+          name="email"
+          value=""
+          placeholder="email"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <input
+          type="password"
+          name="password"
+          value=""
+          placeholder="password"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <button type="submit" className="p-4 my-6 bg-red-700 w-full rounded-lg">
+          {sign ? "Sign in" : "Sign Up"}
+        </button>
+
+        <p className="font-bold cursor-pointer" onClick={toogleSignInform}>
+          {sign ? "New To NetFlix? Sign Up" : "Alereay register! sign in"}
+        </p>
+      </form>
+    </div>
+  );
 }
